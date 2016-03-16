2beec5bea71b86839683517906fd2e0cad43c2cb
Kravis Liu <LeavesNo.67@gmail.com>
2016-03-16 18:03:31 +0800

    Combined play/pause buttons

--- javascripts/music_player.js
+++ javascripts/music_player.js
@@ -1,12 +1,14 @@
 var MusicController = {
 	createNew: function () {
 		var o = {};
-		// 属性
+		// properties
 		o.name = "MusicController";
 
-		// 方法
+		// methods
 		o.initialize = function () {
 			this.setNodeReferences();
+			this.stopDefaultEvents();
+			this.addEventListeners();
 		};
 
 		o.setNodeReferences = function () {
@@ -14,15 +16,53 @@ var MusicController = {
 
 			// slide-bar: 
 			this.progressIndicator = document.querySelector(".progress-indicator");
-			this.sliderRunnableTrack = document.querySelector("slider-runnable-track");
-			this.currentTime = document.querySelector("current-time");
-			this.totalTime = document.querySelector("total-time");
+			this.sliderRunnableTrack = document.querySelector(".slider-runnable-track");
+			this.currentTime = document.querySelector(".current-time");
+			this.totalTime = document.querySelector(".total-time");
 			
 			// control-panel: 
-			
-		}
+			this.forwardSpeed = document.querySelector(".forward-speed");
+			this.backwardSpeed = document.querySelector(".backward-speed");
+			this.forwardButton = document.querySelector(".forward-button");
+			this.playPauseButton = document.querySelector(".play-pause-button");
+			this.backwardButton = document.querySelector(".backward-button");
+		};
 
-		// 返回并执行的函数
+		o.stopDefaultEvents = function () {
+			var Atags = document.querySelectorAll(".control-panel a");
+			for (var i = Atags.length - 1; i >= 0; i--) {
+				Atags[i].onclick = function () {
+					return false;
+				};
+			}
+		};
+
+		o.addEventListeners = function () {
+			// slide-bar: 
+
+
+			// control-panel: 
+			this.forwardButton.addEventListener("click", this.forwardButtonClick);
+			this.playPauseButton.addEventListener("click", this.playPauseButtonClick);
+			this.backwardButton.addEventListener("click", this.backwardButtonClick);
+		};
+
+		o.playPauseButtonClick = function () {
+			o.controlMusic = this.getAttribute("value") === "play" ? o.playMusic : o.pauseMusic;
+			o.controlMusic();
+		};
+
+		o.playMusic = function () {
+			this.playPauseButton.setAttribute("value", "pause");
+			this.audioSource.play();
+		};
+
+		o.pauseMusic = function () {
+			this.playPauseButton.setAttribute("value", "play");
+			this.audioSource.pause();
+		};
+
+		// Execute functions and return object
 		o.initialize();
 		return o;
 	}
